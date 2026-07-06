28559ed4c4b26206eb8fe68cff8b47317ce050c7
Chang <e.gauss@gmail.com>
2026-07-06 23:32:44 +0800

    implement gpt model block

--- main.py
+++ main.py
@@ -1,27 +1,57 @@
 import urllib.request
+
+import tiktoken
+import torch
+
 import dataset
 from model.attention.self_attention import SelfAttention
 from model.embeddings import generate_inputs_embeddings, embed_tokens
+from model.gpt_model import DummyGPTModel
 
+GPT_CONFIG_124M = {
+        "vocab_size": 50257,
+        "context_length": 1024,
+        "emb_dim": 768,
+        "n_heads": 12,
+        "n_layers": 12,
+        "drop_rate": 0.1,
+        "qkv_bias": False
+    }
 
 def main():
-    url = ("https://raw.githubusercontent.com/rasbt/"
-           "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
-           "the-verdict.txt")
+    # url = ("https://raw.githubusercontent.com/rasbt/"
+    #        "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
+    #        "the-verdict.txt")
+    #
+    # with urllib.request.urlopen(url) as response:
+    #     raw_text = response.read().decode("utf-8")
+    #
+    # dataloader = dataset.create_dataloader(raw_text, batch_size=8,
+    #                                       max_length=4, stride=4, shuffle=False)
+    # data_iter = iter(dataloader)
+    # inputs, target = next(data_iter)
+    # # print(inputs)
+    #
+    # print(generate_inputs_embeddings(embed_tokens(inputs)))
+    #
+    # self_attention = SelfAttention(4,2)
+    # print(self_attention(inputs))
+    tokenizer = tiktoken.get_encoding("gpt2")
 
-    with urllib.request.urlopen(url) as response:
-        raw_text = response.read().decode("utf-8")
+    batch = []
+    txt1 = "Every effort moves you"
+    txt2 = "Every day holds a"
 
-    dataloader = dataset.create_dataloader(raw_text, batch_size=8,
-                                          max_length=4, stride=4, shuffle=False)
-    data_iter = iter(dataloader)
-    inputs, target = next(data_iter)
-    # print(inputs)
+    batch.append(torch.tensor(tokenizer.encode(txt1)))
+    batch.append(torch.tensor(tokenizer.encode(txt2)))
+    batch = torch.stack(batch, dim=0)
+    print(batch)
 
-    print(generate_inputs_embeddings(embed_tokens(inputs)))
-
-    self_attention = SelfAttention(4,2)
-    print(self_attention(inputs))
+    torch.manual_seed(123)
+    model = DummyGPTModel(GPT_CONFIG_124M)
+    logits = model(batch)
+    print(logits)
+    print(logits.shape)
 
 
 if __name__ == "__main__":
